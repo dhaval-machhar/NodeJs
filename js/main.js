d511d8f6d3223bc9caf828512d138a1f80527f1a
dhaval-machhar <59963919+dhaval-machhar@users.noreply.github.com>
2020-04-13 19:34:48 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 /*first slider */
-$('.slider-one').slick({
+$(".slider-one")
+.not("slick-initialized")
+.slick({
     autoplay:true,
     autoplaySpeed:3000,
+    prevArrow: ".site-slider.slider-btn.prev",
+    nextArrow: ".site-slider.slider-btn.next",
     dots: true
 });
