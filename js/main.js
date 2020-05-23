5a3f6c06465dc14ee6be37799927fb29de74ad2d
Dhaval Machhar <dhavalmchhr@gmail.com>
2020-05-23 13:31:18 +0530

    completed vintage partially

--- js/main.js
+++ js/main.js
@@ -20,3 +20,40 @@ $(".slider-two")
     nextArrow:".site-slider-two.next",
     autoplay: 3000
 });
+
+$('.slider-three').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $(".slider-four")
+.not("slick-initialized")
+.slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    prevArrow:".site-slider-two.prev",
+    nextArrow:".site-slider-two.next",
+    autoplay: 3000
+});
